d2d70803375df3050676d1b4d5d5c2d96952dd81
Martin Jambor <mjambor@suse.cz>
2014-05-28 17:01:41 +0200

    fixed_pr_branches.py now has -v option

--- python/fixed_pr_branches.py
+++ python/fixed_pr_branches.py
@@ -15,7 +15,50 @@ def die (s):
     sys.exit (1)
     pass
 
+def examine_pr_branch (branch, bugnum, verbose):
+    url = ("https://gcc.gnu.org/bugzilla/show_bug.cgi?ctype=xml&id="
+           + str(bugnum))
+    # print url
+    try:
+        stream = urllib2.urlopen(url)
+        tree = ET.parse(stream)
+        root = tree.getroot()
+        status = root.find("./bug/bug_status").text
+        # resolution = root.find("./bug/resolution").text
+            
+        if status == "RESOLVED":
+            print branch
+            if verbose:
+                shdesc = root.find("./bug/short_desc").text.strip()
+                print (shdesc[:75])
+                print ("https://gcc.gnu.org/bugzilla/show_bug.cgi?id="
+                       + str(bugnum))
+                bug = root.find("bug")
+                lastdesc = bug.findall("long_desc")[-1]
+                when = lastdesc.find("bug_when").text
+                who = lastdesc.find("who")
+                print ("Last comment: {0} {1}"
+                       .format(when, str(who.attrib["name"])))
+                print ("");
+                pass
+            pass
+        stream.close()
+
+    except:
+        sys.stderr.write ("Failed examining branch {0}\n".format(branch))
+        raise
+        return
+    return
+
 def list_fixed_branches_main():
+
+    if (len(sys.argv) >= 2
+        and sys.argv[1] == "-v"):
+        verbose = True
+    else:
+        verbose = False
+        pass
+
 
     p1 = Popen (["git", "branch"], stdout=PIPE)
     o1 = p1.communicate()
@@ -31,26 +74,10 @@ def list_fixed_branches_main():
         if not match:
             continue
 
-#        print match.group(1)
-#        print branch
+        # print match.group(1)
+        # print branch
+        examine_pr_branch (branch, match.group(1), verbose)
 
-        url = ("https://gcc.gnu.org/bugzilla/show_bug.cgi?ctype=xml&id="
-               + str(match.group(1)))
-#        print url
-        try:
-            stream = urllib2.urlopen(url)
-            tree = ET.parse(stream)
-            root = tree.getroot()
-            status = root.find("./bug/bug_status")
-            resolution = root.find("./bug/resolution")
-            stream.close()
-            
-            if status.text == "RESOLVED":
-                print branch
-
-        except:
-            sys.stderr.write ("Failed examining branch {}\n".format(branch))
-            continue
         pass
     pass
 
